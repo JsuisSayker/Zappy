2183b28d7fdef7b06a66d237bfe25a694b06790d
Marc <marc.mendia@epitech.eu>
2024-05-21 16:10:29 +0200

    [ADD && UPD] add macro_server.h and move all macro un zappy_server on macro_server.h

---
+++ ZappyServer/include/macro_server.h
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** macro_server
+*/
+
+#ifndef MACRO_SERVER_H_
+    #define MACRO_SERVER_H_
+    #define SAVE_FILE "myteams_save.txt"
+    #define USERS_CHAR 'u'
+    #define PRIVATE_MESSAGE_CHAR 'm'
+    #define TEAMS_CHAR 't'
+    #define CHANNELS_CHAR 'c'
+    #define THREADS_CHAR 'h'
+    #define REPLY_CHAR 'r'
+    #define SUBSCRIBE_CHAR 's'
+    #define OK 0
+    #define KO 84
+    #define ERROR -1
+    #define FOOD_DENSITY 0.5
+    #define LINEMATE_DENSITY 0.3
+    #define DERAUMERE_DENSITY 0.15
+    #define SIBUR_DENSITY 0.1
+    #define MENDIANE_DENSITY 0.1
+    #define PHIRAS_DENSITY 0.08
+    #define THYSTAME_DENSITY 0.05
+    #define MAX_CLIENTS 10
+
+
+#endif /* !MACRO_SERVER_H_ */
