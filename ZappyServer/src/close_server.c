6ae7fac18917598daa234de1b43c0cd6660b225a
Tom Mendy <tom.mendy@epitech.eu>
2024-05-25 14:25:12 +0200

    [FIX] coding-style server

--- ZappyServer/src/close_server.c
+++ ZappyServer/src/close_server.c
@@ -7,14 +7,13 @@
 
 #include <zappy_server.h>
 
-static int send_logout_to_all_clients(UNUSED zappy_server_t *zappy_server)
+static int send_logout_to_all_clients(zappy_server_t *zappy_server)
 {
     for (zappy_server->actual_sockfd = 0;
         zappy_server->actual_sockfd < __FD_SETSIZE;
         zappy_server->actual_sockfd += 1) {
         if (zappy_server->clients[zappy_server->actual_sockfd].team_name)
             free(zappy_server->clients[zappy_server->actual_sockfd].team_name);
-    //         return ERROR;
     }
     return OK;
 }
