cf4f9da2e7ff3c5c0085ecb19ddb49a3f8d0baf1
Tom Mendy <tom.mendy@epitech.eu>
2024-05-30 10:45:32 +0200

    [ADD] refill_map_tile

--- ZappyServer/src/map_tile_function_sub.c
+++ ZappyServer/src/map_tile_function_sub.c
@@ -73,3 +73,32 @@ map_tile_t **copy_map_tile(map_tile_t **source)
     destination[len_y] = NULL;
     return destination;
 }
+
+
+void refill_inventory(inventory_t *destination, inventory_t *source)
+{
+    if (destination->food < source->food)
+        destination->food = source->food;
+    if (destination->linemate < source->linemate)
+        destination->linemate = source->linemate;
+    if (destination->deraumere < source->deraumere)
+        destination->deraumere = source->deraumere;
+    if (destination->sibur < source->sibur)
+        destination->sibur = source->sibur;
+    if (destination->mendiane < source->mendiane)
+        destination->mendiane = source->mendiane;
+    if (destination->phiras < source->phiras)
+        destination->phiras = source->phiras;
+    if (destination->thystame < source->thystame)
+        destination->thystame = source->thystame;
+}
+
+void refill_map_tile(map_tile_t **destination, map_tile_t **source)
+{
+    for (int i = 0; source[i] != NULL; i += 1) {
+        for (int j = 0; source[i][j].x != -1; j += 1) {
+            refill_inventory(&destination[i][j].inventory,
+                &source[i][j].inventory);
+        }
+    }
+}
