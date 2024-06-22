d80c05d2a5a39073aa00429386a87c338ba00d46
Tom Mendy <tom.mendy@epitech.eu>
2024-06-22 20:28:06 +0200

    [FIX] server command

--- ZappyServer/src/CommandsServer/server_command_clear.c
+++ ZappyServer/src/CommandsServer/server_command_clear.c
@@ -7,7 +7,9 @@
 
 #include <zappy_server.h>
 
-void server_command_clear(UNUSED zappy_server_t *zappy, UNUSED char *command)
+void server_command_clear(zappy_server_t *zappy, char *command)
 {
+    if (zappy == NULL || command == NULL || command[0] != '\0')
+        return;
     write(STDOUT_FILENO, CLEAR_SCREEN, strlen(CLEAR_SCREEN));
 }
