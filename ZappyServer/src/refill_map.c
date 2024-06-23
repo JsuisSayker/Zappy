cc2442140ce6d3e612722b8c3e94034ee13bc4da
Tom Mendy <tom.mendy@epitech.eu>
2024-06-23 16:37:15 +0200

    [FIX] refill

--- ZappyServer/src/refill_map.c
+++ ZappyServer/src/refill_map.c
@@ -7,22 +7,44 @@
 
 #include <zappy_server.h>
 
+int nb_resource_tile(inventory_t *inventory)
+{
+    int nb = 0;
+
+    nb += inventory->food;
+    nb += inventory->linemate;
+    nb += inventory->deraumere;
+    nb += inventory->sibur;
+    nb += inventory->mendiane;
+    nb += inventory->phiras;
+    nb += inventory->thystame;
+    return nb;
+}
+
 static void add_resource_1(zappy_server_t *zappy, bool *is_refill)
 {
+    int x = rand() % zappy->args->width;
+    int y = rand() % zappy->args->height;
     int resource_to_add = (FOOD_DENSITY * zappy->args->width *
         zappy->args->height / 100) - zappy->all_resources[0];
 
     for (int i = 0; i < resource_to_add; i += 1) {
-        zappy->map_tile[rand() % zappy->args->height]
-            [rand() % zappy->args->width].inventory.food += 1;
+        while (nb_resource_tile(&zappy->map_tile[y][x].inventory) > 2){
+            x = rand() % zappy->args->height;
+            y = rand() % zappy->args->width;
+        }
+        zappy->map_tile[y][x].inventory.food += 1;
         zappy->all_resources[0] += 1;
         (*is_refill) = true;
     }
     resource_to_add = (LINEMATE_DENSITY * zappy->args->width *
             zappy->args->height / 100) - zappy->all_resources[1];
     for (int i = 0; i < resource_to_add; i += 1) {
-        zappy->map_tile[rand() % zappy->args->height]
-            [rand() % zappy->args->width].inventory.linemate += 1;
+        while (nb_resource_tile(&zappy->map_tile[y][x].inventory) > 2){
+            x = rand() % zappy->args->height;
+            y = rand() % zappy->args->width;
+        }
+        zappy->map_tile[y][x].inventory.linemate += 1;
         zappy->all_resources[1] += 1;
         (*is_refill) = true;
     }
@@ -30,20 +52,28 @@ static void add_resource_1(zappy_server_t *zappy, bool *is_refill)
 
 static void add_resource_2(zappy_server_t *zappy, bool *is_refill)
 {
+    int x = rand() % zappy->args->width;
+    int y = rand() % zappy->args->height;
     int resource_to_add = (DERAUMERE_DENSITY * zappy->args->width *
             zappy->args->height / 100) - zappy->all_resources[2];
 
     for (int i = 0; i < resource_to_add; i += 1) {
-        zappy->map_tile[rand() % zappy->args->height]
-            [rand() % zappy->args->width].inventory.deraumere += 1;
+        while (nb_resource_tile(&zappy->map_tile[y][x].inventory) > 2){
+            x = rand() % zappy->args->height;
+            y = rand() % zappy->args->width;
+        }
+        zappy->map_tile[y][x].inventory.deraumere += 1;
         zappy->all_resources[2] += 1;
         (*is_refill) = true;
     }
     resource_to_add = (SIBUR_DENSITY * zappy->args->width *
         zappy->args->height / 100) - zappy->all_resources[3];
     for (int i = 0; i < resource_to_add; i += 1) {
-        zappy->map_tile[rand() % zappy->args->height]
-            [rand() % zappy->args->width].inventory.sibur += 1;
+        while (nb_resource_tile(&zappy->map_tile[y][x].inventory) > 2){
+            x = rand() % zappy->args->height;
+            y = rand() % zappy->args->width;
+        }
+        zappy->map_tile[y][x].inventory.sibur += 1;
         zappy->all_resources[3] += 1;
         (*is_refill) = true;
     }
@@ -51,20 +81,28 @@ static void add_resource_2(zappy_server_t *zappy, bool *is_refill)
 
 static void add_resource_3(zappy_server_t *zappy, bool *is_refill)
 {
+    int x = rand() % zappy->args->width;
+    int y = rand() % zappy->args->height;
     int resource_to_add = (MENDIANE_DENSITY * zappy->args->width *
         zappy->args->height / 100) - zappy->all_resources[4];
 
     for (int i = 0; i < resource_to_add; i += 1) {
-        zappy->map_tile[rand() % zappy->args->height]
-            [rand() % zappy->args->width].inventory.mendiane += 1;
+        while (nb_resource_tile(&zappy->map_tile[y][x].inventory) > 2){
+            x = rand() % zappy->args->height;
+            y = rand() % zappy->args->width;
+        }
+        zappy->map_tile[y][x].inventory.mendiane += 1;
         zappy->all_resources[4] += 1;
         (*is_refill) = true;
     }
     resource_to_add = (PHIRAS_DENSITY * zappy->args->width *
         zappy->args->height / 100) - zappy->all_resources[5];
     for (int i = 0; i < resource_to_add; i += 1) {
-        zappy->map_tile[rand() % zappy->args->height]
-            [rand() % zappy->args->width].inventory.phiras += 1;
+        while (nb_resource_tile(&zappy->map_tile[y][x].inventory) > 2){
+            x = rand() % zappy->args->height;
+            y = rand() % zappy->args->width;
+        }
+        zappy->map_tile[y][x].inventory.phiras += 1;
         zappy->all_resources[5] += 1;
         (*is_refill) = true;
     }
@@ -72,12 +110,17 @@ static void add_resource_3(zappy_server_t *zappy, bool *is_refill)
 
 static void add_resource_4(zappy_server_t *zappy, bool *is_refill)
 {
+    int x = rand() % zappy->args->width;
+    int y = rand() % zappy->args->height;
     int resource_to_add = (THYSTAME_DENSITY * zappy->args->width *
             zappy->args->height / 100) - zappy->all_resources[6];
 
     for (int i = 0; i < resource_to_add; i += 1) {
-        zappy->map_tile[rand() % zappy->args->height]
-            [rand() % zappy->args->width].inventory.phiras += 1;
+        while (nb_resource_tile(&zappy->map_tile[y][x].inventory) > 2){
+            x = rand() % zappy->args->height;
+            y = rand() % zappy->args->width;
+        }
+        zappy->map_tile[y][x].inventory.thystame += 1;
         zappy->all_resources[6] += 1;
         (*is_refill) = true;
     }
